c378d17f8d8084510a255da39cfaf8c9866e2da2
Yaroslav <yaroslav.shebovovkk@gmail.com>
2025-05-04 14:08:11 +0200

    added link to the leaderboard inside header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ export default function Header({isRunning, setIsRunning}) {
     return (
         <header className={styles.header}>
             <Link to="/">Where's Waldo</Link>
+            <Link to="/leaderboard">Leaderboard</Link>
             {isRunning && <Timer isRunning={isRunning} setIsRunning={setIsRunning} />}
             
         </header>
